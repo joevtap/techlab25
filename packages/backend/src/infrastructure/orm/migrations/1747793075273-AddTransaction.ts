ae824ef528fc66a0bdeaee37809de6c36fba5a71
joevtap <joelvtorres2024@gmail.com>
2025-05-22 13:37:10 -0300

    style: format

--- packages/backend/src/infrastructure/orm/migrations/1747793075273-AddTransaction.ts
+++ packages/backend/src/infrastructure/orm/migrations/1747793075273-AddTransaction.ts
@@ -1,26 +1,49 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddTransaction1747793075273 implements MigrationInterface {
-    name = 'AddTransaction1747793075273'
+  name = 'AddTransaction1747793075273';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TYPE "public"."transactions_type_enum" AS ENUM('CREDIT', 'DEBIT')`);
-        await queryRunner.query(`CREATE TABLE "transactions" ("id" text NOT NULL, "type" "public"."transactions_type_enum" NOT NULL, "from_id" text NOT NULL, "to_id" text NOT NULL, "value" integer NOT NULL, "description" text, "created_at" TIMESTAMP NOT NULL, CONSTRAINT "PK_a219afd8dd77ed80f5a862f1db9" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "accounts" DROP CONSTRAINT "FK_e6c1947a61f955558ccca3f7c46"`);
-        await queryRunner.query(`ALTER TABLE "accounts" ALTER COLUMN "owner_id" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "accounts" ADD CONSTRAINT "FK_e6c1947a61f955558ccca3f7c46" FOREIGN KEY ("owner_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "transactions" ADD CONSTRAINT "FK_811faa40e043801b0a4b3737d27" FOREIGN KEY ("from_id") REFERENCES "accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "transactions" ADD CONSTRAINT "FK_7f681ce46f24957781c744c3561" FOREIGN KEY ("to_id") REFERENCES "accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "public"."transactions_type_enum" AS ENUM('CREDIT', 'DEBIT')`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "transactions" ("id" text NOT NULL, "type" "public"."transactions_type_enum" NOT NULL, "from_id" text NOT NULL, "to_id" text NOT NULL, "value" integer NOT NULL, "description" text, "created_at" TIMESTAMP NOT NULL, CONSTRAINT "PK_a219afd8dd77ed80f5a862f1db9" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "accounts" DROP CONSTRAINT "FK_e6c1947a61f955558ccca3f7c46"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "accounts" ALTER COLUMN "owner_id" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "accounts" ADD CONSTRAINT "FK_e6c1947a61f955558ccca3f7c46" FOREIGN KEY ("owner_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transactions" ADD CONSTRAINT "FK_811faa40e043801b0a4b3737d27" FOREIGN KEY ("from_id") REFERENCES "accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transactions" ADD CONSTRAINT "FK_7f681ce46f24957781c744c3561" FOREIGN KEY ("to_id") REFERENCES "accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "transactions" DROP CONSTRAINT "FK_7f681ce46f24957781c744c3561"`);
-        await queryRunner.query(`ALTER TABLE "transactions" DROP CONSTRAINT "FK_811faa40e043801b0a4b3737d27"`);
-        await queryRunner.query(`ALTER TABLE "accounts" DROP CONSTRAINT "FK_e6c1947a61f955558ccca3f7c46"`);
-        await queryRunner.query(`ALTER TABLE "accounts" ALTER COLUMN "owner_id" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "accounts" ADD CONSTRAINT "FK_e6c1947a61f955558ccca3f7c46" FOREIGN KEY ("owner_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`DROP TABLE "transactions"`);
-        await queryRunner.query(`DROP TYPE "public"."transactions_type_enum"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "transactions" DROP CONSTRAINT "FK_7f681ce46f24957781c744c3561"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transactions" DROP CONSTRAINT "FK_811faa40e043801b0a4b3737d27"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "accounts" DROP CONSTRAINT "FK_e6c1947a61f955558ccca3f7c46"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "accounts" ALTER COLUMN "owner_id" DROP NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "accounts" ADD CONSTRAINT "FK_e6c1947a61f955558ccca3f7c46" FOREIGN KEY ("owner_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(`DROP TABLE "transactions"`);
+    await queryRunner.query(`DROP TYPE "public"."transactions_type_enum"`);
+  }
 }
--- packages/backend/src/infrastructure/orm/migrations/1747793979952-TransactionTypeTransfer.ts
+++ packages/backend/src/infrastructure/orm/migrations/1747793979952-TransactionTypeTransfer.ts
@@ -1,32 +1,69 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class TransactionTypeTransfer1747793979952 implements MigrationInterface {
-    name = 'TransactionTypeTransfer1747793979952'
+export class TransactionTypeTransfer1747793979952
+  implements MigrationInterface
+{
+  name = 'TransactionTypeTransfer1747793979952';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "transactions" DROP CONSTRAINT "FK_811faa40e043801b0a4b3737d27"`);
-        await queryRunner.query(`ALTER TABLE "transactions" DROP CONSTRAINT "FK_7f681ce46f24957781c744c3561"`);
-        await queryRunner.query(`ALTER TYPE "public"."transactions_type_enum" RENAME TO "transactions_type_enum_old"`);
-        await queryRunner.query(`CREATE TYPE "public"."transactions_type_enum" AS ENUM('CREDIT', 'DEBIT', 'TRANSFER')`);
-        await queryRunner.query(`ALTER TABLE "transactions" ALTER COLUMN "type" TYPE "public"."transactions_type_enum" USING "type"::"text"::"public"."transactions_type_enum"`);
-        await queryRunner.query(`DROP TYPE "public"."transactions_type_enum_old"`);
-        await queryRunner.query(`ALTER TABLE "transactions" ALTER COLUMN "from_id" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "transactions" ALTER COLUMN "to_id" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "transactions" ADD CONSTRAINT "FK_811faa40e043801b0a4b3737d27" FOREIGN KEY ("from_id") REFERENCES "accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "transactions" ADD CONSTRAINT "FK_7f681ce46f24957781c744c3561" FOREIGN KEY ("to_id") REFERENCES "accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "transactions" DROP CONSTRAINT "FK_811faa40e043801b0a4b3737d27"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transactions" DROP CONSTRAINT "FK_7f681ce46f24957781c744c3561"`,
+    );
+    await queryRunner.query(
+      `ALTER TYPE "public"."transactions_type_enum" RENAME TO "transactions_type_enum_old"`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."transactions_type_enum" AS ENUM('CREDIT', 'DEBIT', 'TRANSFER')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transactions" ALTER COLUMN "type" TYPE "public"."transactions_type_enum" USING "type"::"text"::"public"."transactions_type_enum"`,
+    );
+    await queryRunner.query(`DROP TYPE "public"."transactions_type_enum_old"`);
+    await queryRunner.query(
+      `ALTER TABLE "transactions" ALTER COLUMN "from_id" DROP NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transactions" ALTER COLUMN "to_id" DROP NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transactions" ADD CONSTRAINT "FK_811faa40e043801b0a4b3737d27" FOREIGN KEY ("from_id") REFERENCES "accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transactions" ADD CONSTRAINT "FK_7f681ce46f24957781c744c3561" FOREIGN KEY ("to_id") REFERENCES "accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "transactions" DROP CONSTRAINT "FK_7f681ce46f24957781c744c3561"`);
-        await queryRunner.query(`ALTER TABLE "transactions" DROP CONSTRAINT "FK_811faa40e043801b0a4b3737d27"`);
-        await queryRunner.query(`ALTER TABLE "transactions" ALTER COLUMN "to_id" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "transactions" ALTER COLUMN "from_id" SET NOT NULL`);
-        await queryRunner.query(`CREATE TYPE "public"."transactions_type_enum_old" AS ENUM('CREDIT', 'DEBIT')`);
-        await queryRunner.query(`ALTER TABLE "transactions" ALTER COLUMN "type" TYPE "public"."transactions_type_enum_old" USING "type"::"text"::"public"."transactions_type_enum_old"`);
-        await queryRunner.query(`DROP TYPE "public"."transactions_type_enum"`);
-        await queryRunner.query(`ALTER TYPE "public"."transactions_type_enum_old" RENAME TO "transactions_type_enum"`);
-        await queryRunner.query(`ALTER TABLE "transactions" ADD CONSTRAINT "FK_7f681ce46f24957781c744c3561" FOREIGN KEY ("to_id") REFERENCES "accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "transactions" ADD CONSTRAINT "FK_811faa40e043801b0a4b3737d27" FOREIGN KEY ("from_id") REFERENCES "accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "transactions" DROP CONSTRAINT "FK_7f681ce46f24957781c744c3561"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transactions" DROP CONSTRAINT "FK_811faa40e043801b0a4b3737d27"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transactions" ALTER COLUMN "to_id" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transactions" ALTER COLUMN "from_id" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."transactions_type_enum_old" AS ENUM('CREDIT', 'DEBIT')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transactions" ALTER COLUMN "type" TYPE "public"."transactions_type_enum_old" USING "type"::"text"::"public"."transactions_type_enum_old"`,
+    );
+    await queryRunner.query(`DROP TYPE "public"."transactions_type_enum"`);
+    await queryRunner.query(
+      `ALTER TYPE "public"."transactions_type_enum_old" RENAME TO "transactions_type_enum"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transactions" ADD CONSTRAINT "FK_7f681ce46f24957781c744c3561" FOREIGN KEY ("to_id") REFERENCES "accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transactions" ADD CONSTRAINT "FK_811faa40e043801b0a4b3737d27" FOREIGN KEY ("from_id") REFERENCES "accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
 }
