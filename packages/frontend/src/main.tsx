d698326fd895ad984d04d3355e50b233c0e829b7
joevtap <joelvtorres2024@gmail.com>
2025-05-24 11:22:14 -0300

    feat: add login form base, setup theme and fonts

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
 
+import '@fontsource/geist-sans/400.css';
+import '@fontsource/geist-sans/500.css';
+import '@fontsource/geist-sans/600.css';
+import '@fontsource/geist-sans/700.css';
+import '@fontsource-variable/merriweather/index.css';
+import '@fontsource-variable/jetbrains-mono/index.css';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
